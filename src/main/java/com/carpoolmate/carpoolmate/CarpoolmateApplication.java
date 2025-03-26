d4c38ee1d26df7cb84f73adf38433ef7d4fe6855
Jan Cermak <honzacermak74@gmail.com>
2025-03-26 18:14:51 +0100

    initial commit

---
+++ src/main/java/com/carpoolmate/carpoolmate/CarpoolmateApplication.java
@@ -0,0 +1,13 @@
+package com.carpoolmate.carpoolmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpoolmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarpoolmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carpoolmate/carpoolmate/CarpoolmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carpoolmate.carpoolmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpoolmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
